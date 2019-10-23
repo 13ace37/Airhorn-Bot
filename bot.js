4e4fa687b38bd2d636f050deb1a755807513b84c
Bram Reuling <bram.reuling@outlook.com>
2019-10-23 11:43:39 +0200

    Initial Commit
    Committed the project so far

---
+++ bot.js
@@ -0,0 +1,40 @@
+const { Client, RichEmbed } = require('discord.js');
+const client = new Client();
+
+const auth = require('./auth.json');
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setActivity('/join');
+});
+
+client.on('message', msg => {
+    if (!msg.guild) return;
+
+    if (msg.content === '/join') {
+        if (msg.member.voiceChannel) {
+            msg.member.voiceChannel.join()
+                .then(connection => {
+                    msg.delete();
+                    const dispatcher = connection.playFile('E:/Projecten/Bots/test-bot/sounds/airhorn.mp3');
+                    dispatcher.on('end', () => {
+                        msg.member.voiceChannel.leave();
+                    });
+
+                    dispatcher.on('error', e => {
+                        console.log(e);
+                    });
+                })
+                .catch(console.log);
+        }
+    }
+
+    // if (msg.content === '/leave') {
+    //     if (msg.member.voiceChannel) {
+    //         msg.member.voiceChannel.leave()
+    //     }
+    // }
+
+});
+
+client.login(auth.token);
